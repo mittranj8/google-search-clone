c00a71f88aa71d1cce4d81980dc88760fff1c48b
Mittranj <mpansuriya@unomaha.edu>
2020-10-05 23:44:31 -0500

    added intial firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_A_09NaJR-MPwfGUrOGvdBQhYlCTAySI",
+  authDomain: "ggle-clone-react.firebaseapp.com",
+  databaseURL: "https://ggle-clone-react.firebaseio.com",
+  projectId: "ggle-clone-react",
+  storageBucket: "ggle-clone-react.appspot.com",
+  messagingSenderId: "865423222166",
+  appId: "1:865423222166:web:5f8f24180b33e9f064e9b7",
+  measurementId: "G-88RE5XVDKL",
+};
