67656f5a001cc0229114027501b79c8195fbe866
Mittranj <mpansuriya@unomaha.edu>
2020-10-09 14:24:10 -0500

    added custom hook for api calls

---
+++ src/pages/useGoogleSearch.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import API_KEY from "./keys";
+
+const CONTEXT_KEY = "73fb5753b83496789";
+
+// custom hook
+const useGoogleSearch = (term) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
+    fetchData();
+  }, [term]);
+  return { data };
+};
+
+export default useGoogleSearch;
