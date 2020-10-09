ef14da7aa41938044e9d63d799f4a4e3be77eb8b
Mittranj <mpansuriya@unomaha.edu>
2020-10-09 14:25:00 -0500

    added saved response for mocking API calls

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { useStateValue } from "../StateProvider";
+import Response from "./response";
 import "./SearchPage.css";
+import useGoogleSearch from "./useGoogleSearch";
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
 
+  //   LIVE API Call
+  //   const { data } = useGoogleSearch(term);
+
+  //   using saved reponse to save custom search limit (mocked API call)
+  const data = Response;
+
+  console.log(data);
+
+  //  https://developers.google.com/custom-search/v1/introduction#identify_your_application_to_google_with_api_key
+  //  https://cse.google.com/cse/create/new
   return (
     <div className="searchPage">
       <div className="searchPage_header">
